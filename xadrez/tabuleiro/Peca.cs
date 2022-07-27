818bd589b4d773cc432ba0c0d8b02f181240500a
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2022-07-27 20:06:17 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez/tabuleiro/Peca.cs
+++ xadrez/tabuleiro/Peca.cs
@@ -17,5 +17,10 @@ namespace tabuleiro
             this.qteMovimentos = 0;
             
         }
+
+        public void incrementrarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
