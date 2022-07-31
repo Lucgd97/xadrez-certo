063263747bcd1ee3d71c3ae65bda794ebfd777d5
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2022-07-31 16:13:04 -0300

    Implementando a logica de xequemate

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -44,6 +44,8 @@ namespace xadrez
 
                     }
                 }
+                Console.Clear ();
+                Tela.imprimirPartida(partida);
 
             }
             catch (TabuleiroException e)
